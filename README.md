21fbbe3fec3b0ae66ca0a1b87657bb98f3aceca3
Martin Schmitt <mnschmit@users.noreply.github.com>
2023-05-08 16:48:51 +0200

    Initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# python-code-style
-A GitHub action that helps python code to adhere to our style guide
+# Python Code Style
+
+A GitHub action that helps python code to adhere
+to our style guide for python code at celebrate company.
+
+## Linters
+
+- flake8
+- mypy
+- pylint
+
+## Usage
+
+This action ONLY runs the linters.
+The python environment should be set up separately.
+
+```yaml
+name: celebrate python linting
+uses: kartenmacherei/python-code-style@0.0.1
+```
+
+## Features
+
+- Runs python linters one after the other; fails with the first failing linter
+- If triggered by a pull request, linter results will be added as comments to the PR.
---
+++ linting_orchestrator.py
@@ -0,0 +1,61 @@
+"""
+This script redirects the linting for flake8 and pylint to lintly
+if we lint a PR and otherwise fails if the specified linter fails.
+"""
+
+import argparse
+import os
+import sys
+import subprocess
+
+LINTER_TO_CMD = {
+    "flake8": ["flake8", "--extend-exclude=dist,build"],
+    "pylint": ["pylint", "--output-format=json", "--rcfile=.pylintrc"],
+}
+LINTER_TO_FORMAT = {"flake8": "flake8", "pylint": "pylint-json"}
+
+LINTLY_CMD = [
+    "lintly",
+    "--api-key",
+    os.environ["GITHUB_TOKEN"],
+    "--fail-on",
+    "any",
+    "--post-status",
+    "--request-changes",
+    "--use-checks",
+    "--no-exit-zero",
+]
+
+
+def main(args: argparse.Namespace) -> None:
+    if not args.python_files:
+        print("No lintable python files. Exiting.")
+        sys.exit(0)
+    else:
+        LINTER_TO_CMD[args.linter].extend(args.python_files)
+
+    linting_process = subprocess.run(
+        LINTER_TO_CMD[args.linter], capture_output=True, check=False
+    )
+
+    if args.is_pull_request == "true":
+        LINTLY_CMD.append("--format=" + LINTER_TO_FORMAT[args.linter])
+        lintly_process = subprocess.run(
+            LINTLY_CMD, input=linting_process.stdout, check=False
+        )
+        sys.exit(lintly_process.returncode)
+    else:
+        if linting_process.returncode == 0:
+            print("Success: No issues found.")
+        else:
+            print(linting_process.stdout.decode("utf-8"))
+        sys.exit(linting_process.returncode)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("is_pull_request", choices=["true", "false"])
+    parser.add_argument("linter", choices=LINTER_TO_CMD.keys())
+    parser.add_argument("python_files", nargs="+", metavar="python_file")
+    args = parser.parse_args()
+    main(args)
